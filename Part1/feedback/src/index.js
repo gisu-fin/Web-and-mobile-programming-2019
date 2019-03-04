fff919525fa43cae0bdb9eaaa310971a3f0fe18d
Mirva <mirva.tapola@gmail.com>
2019-03-04 19:42:34 +0200

    Exercise 1.10

--- Part1/feedback/src/index.js
+++ Part1/feedback/src/index.js
@@ -3,27 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const Button = ({handleClick, text}) => (
-    <button onClick = {handleClick}>
-        {text}
-    </button>
+    <button onClick = {handleClick}> {text} </button>
 )
 
 const Statistics = ({hyva, neutraali, huono, keski, prosentti}) => (
-    <div>   
-        <Statistic text="Hyvä" counter={hyva} />
-        <Statistic text="Neutraali" counter={neutraali} />
-        <Statistic text="Huono" counter={huono} />
-        <Statistic text="Keskiarvo" counter={keski}/>
-        <Statistic text= "Positiivisia" counter={prosentti} /> 
-    </div>
+    <table>
+        <tbody> 
+            <Statistic text="Hyvä" counter={hyva} />
+            <Statistic text="Neutraali" counter={neutraali} />
+            <Statistic text="Huono" counter={huono} />
+            <Statistic text="Keskiarvo" counter={keski}/>
+            <Statistic text="Positiivisia" counter={prosentti} /> 
+        </tbody>  
+    </table>
 )
 
 const Statistic = (props) => (
-        <p>{props.text} {props.counter}</p>
-    
+        <tr><td>{props.text}</td><td>{props.counter}</td></tr>    
 )
 
-const Historia = ({hyva, neutraali, huono, keski, prosentti,kaikki}) => {
+const Historia = ({hyva, neutraali, huono, keski, prosentti, kaikki}) => {
     if (kaikki === 0){
         return(
             <div>
@@ -32,11 +31,10 @@ const Historia = ({hyva, neutraali, huono, keski, prosentti,kaikki}) => {
         )
     }
     return (
-            <div>
-                <Statistics hyva={hyva} neutraali={neutraali} huono={huono} keski={keski} prosentti={prosentti}/>
-            </div>
+        <div>
+            <Statistics hyva={hyva} neutraali={neutraali} huono={huono} keski={keski} prosentti={prosentti}/>
+        </div>
     )
-
 }
 
 class App extends React.Component {
@@ -68,8 +66,8 @@ class App extends React.Component {
     }
   
     render() {
-        //sijoitetaan nämä tänne olettaen että päivittyvät paremmin
         const {hyva, neutraali, huono} = this.state
+        //lasketaan kaikki yhteen
         const kaikki = (hyva + neutraali + huono) 
         //lasketaan keskiarvo
         const keski = ((hyva*1 + neutraali*0 + huono*(-1))/kaikki).toFixed(2)
@@ -79,17 +77,15 @@ class App extends React.Component {
         return (
             <div>
                 <h1>Anna palautetta</h1>
-                    <Button handleClick={this.klikHyva} text = "hyvä"/>
-                    <Button handleClick={this.klikNeutraali} text = "neutraali"/>
-                    <Button handleClick={this.klikHuono} text = "huono"/>
+                    <Button handleClick={this.klikHyva} text = "Hyvä"/>
+                    <Button handleClick={this.klikNeutraali} text = "Neutraali"/>
+                    <Button handleClick={this.klikHuono} text = "Huono"/>
                     
                 <h1>Statistiikka</h1>
                     <div>
                         <Historia kaikki={kaikki} hyva={hyva} neutraali={neutraali} huono={huono} keski={keski} prosentti={prosentti}/>
-                    </div>
-                    
+                    </div>                    
             </div>
-
       )
     }
   }
