4d4838bc54f1dfbab95336d08464cc206acbd120
Mirva <mirva.tapola@gmail.com>
2019-03-14 13:27:19 +0200

    Exercise 2.1 works

--- Part2/course/src/index.js
+++ Part2/course/src/index.js
@@ -2,74 +2,66 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-
-const Header = (props) => {
-  return(
-    <h1>{props.course.name}</h1>
-  )
-}
-
-const Contents = ({parts}) => {
-    const mapped = () => parts.map(part => <Part key = {part.id} part = {part} />)
-  return(
-    <div>
-      {/*<Part part={props.course.parts[0]}  />
-      <Part part={props.course.parts[1]}  />
-  <Part part={props.course.parts[2]}  />*/}
-      {mapped()}
-    </div>  
-  )
-}
-
-const Part = (part) => {
-  return(
-    <p>{part.name}: {part.exercises}</p>
-  )
-}
-
-const Total = (props) => {
-  return(
-    <p>Total {props.parts[0].exercises+props.parts[1].exercises+props.parts[2].exercises} exercises</p>
-  )
-}
-
-
 const Course = ({course}) => {
     return (
         <div>
         <Header course = {course}/>
-        <Contents course = {course.parts}/>
+        <Contents parts = {course.parts}/>
         </div>
     )    
 }
 
+const Header = ({course}) => {
+  return(
+    <h1>{course.name}</h1>
+  )
+}
+
+const Contents = ({parts}) => {
+  return(
+    <div>
+      {parts.map(part => <p key = {part.id}>{part.name} {part.exercises}</p>)}
+    </div>  
+  )
+}
+
+/*const Part = ({part}) => {
+  return(
+    <p>{part.name} {part.exercises}</p>
+  )
+}
+
+const Total = (parts) => {
+  return(
+    <p>Total {parts[0].exercises+parts[1].exercises+parts[2].exercises} exercises </p>
+  )
+}*/
 
 const App = () => {
-  const course = {
-    name: 'Superadvanced web and mobile programming',
-    parts: [
-      {
-        name: 'Basics of React',
-        exercises: 8,
-        id: 1
-      },
-      {
-        name: 'Using props',
-        exercises: 10,
-        id: 2
-      },
-      {
-        name: 'Component states',
-        exercises: 12,
-        id: 3
-      }
-    ]
-  }
-
+    const course = {
+      name: 'Superadvanced web and mobile programming',
+      parts: [
+        {
+          name: 'Basics of React',
+          exercises: 8,
+          id: 1
+        },
+        {
+          name: 'Using props',
+          exercises: 10,
+          id: 2
+        },
+        {
+          name: 'Component states',
+          exercises: 12,
+          id: 3
+        }
+      ]
+    }
 
   return (
     <div>
-        <Course course = {course} />
+      <Course course = {course} />
     </div>
   )
 }
@@ -78,3 +70,4 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 )
+
