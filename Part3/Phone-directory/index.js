af8ea780f37f118df63060b9f193a4b595eeb71b
Mirva <mirva.tapola@gmail.com>
2019-03-21 22:11:28 +0200

    Tehtävä 3.2 tehty - toimii osoitteessa 3001/persons

---
+++ Part3/Phone-directory/index.js
@@ -0,0 +1,52 @@
+console.log ('Moro')
+
+const express = require('express')
+const app = express()
+
+let persons = [
+      {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+      },
+      {
+        "name": "Martti Tienari",
+        "number": "040-123456",
+        "id": 2
+      },
+      {
+        "name": "Arto Järvinen",
+        "number": "040-123456",
+        "id": 3
+      },
+      {
+        "name": "Lea Kutvonen",
+        "number": "040-123456",
+        "id": 4
+      }
+]
+
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id )
+
+    if ( person ) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+    }
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
